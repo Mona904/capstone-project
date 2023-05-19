68e56c02d10da2157bf9463507f670bb0d758a80
Mona Nogens <m.nogens@gmx.de>
2023-05-19 12:09:26 +0200

    refractoring fav page

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,5 +1,28 @@
-import FavoritesPage from "@/components/FavoritesPage";
+import PlantList from "@/components/PlantList";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+const BackButton = styled.button`
+  margin-top: 1rem;
+  background-color: #173e14;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  cursor: pointer;
+  border-radius: 0.5rem;
+`;
 
 export default function Favorites({ plants, toggleFavorite }) {
-  return <FavoritesPage plants={plants} toggleFavorite={toggleFavorite} />;
+  const favorites = plants.filter((plant) => plant.isFavorite);
+  const router = useRouter();
+
+  return (
+    <>
+      <h1>Favorites</h1>
+
+      <PlantList plants={favorites} toggleFavorite={toggleFavorite} />
+
+      <BackButton onClick={() => router.back()}>Back to Gallery</BackButton>
+    </>
+  );
 }
