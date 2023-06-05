d82f25f15f58fc7795fb5bd48ee87742258f7d73
Mona Nogens <m.nogens@gmx.de>
2023-06-05 12:15:20 +0200

    add plants

--- db.js
+++ db.js
@@ -82,4 +82,114 @@ export const plantData = [
     image:
       "https://cdn.shopify.com/s/files/1/0557/0657/products/Quercus-robur.jpg",
   },
+
+  {
+    id: "9",
+    name: "China Rose",
+    water: "Every 1-2 weeks",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Munich",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/202648501331689472.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "10",
+    name: "Garden Tulip",
+    water: "Every 1-2 weeks",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Cologne",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/154043567663218714.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "11",
+    name: "Ditch Lily",
+    water: "Every 1-2 weeks",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Cologne",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/217198136840617984.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "12",
+    name: "Common Lilac",
+    water: "Every 1-2 weeks",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Hamburg",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/153449182844157981.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "13",
+    name: "Johnny Jump Up",
+    water: "Twice a week",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Munich",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/154040007135330320.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "14",
+    name: "Carnation",
+    water: "Every week",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Cologne",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/153723133038166047.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "15",
+    name: "Japanese Camellia",
+    water: "Every 1-2 weeks",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Hamburg",
+    sunlight: "Partial sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/348098389158035456.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "16",
+    name: "Madagascar Periwinkle",
+    water: "Twice a week",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Cologne",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/202698610715131904.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "17",
+    name: "Busy Lizzie",
+    water: "Twice per week",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Munich",
+    sunlight: "Partial sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/347107359814221824.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
+
+  {
+    id: "18",
+    name: "African Marigold",
+    water: "Twice per week",
+    soil: "Loam, Clay, Sand, Chalky",
+    location: "Hamburg",
+    sunlight: "Full sun",
+    image:
+      "https://www.picturethisai.com/image-handle/website_cmsname/image/1080/315097866204971008.jpeg?x-oss-process=image/format,webp/resize,s_788&v=1.0",
+  },
 ];
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   display: flex;
@@ -12,33 +12,54 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const Image = styled.img`
-  width: 300px;
-  height: auto;
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
-const Text = styled.p`
-  font-size: 1.5rem;
+const PlantPilotText = styled.h1`
+  font-size: 3rem;
   margin-top: 1rem;
-  color: white;
+  color: #f2e8dc;
+  animation: ${fadeIn} 1s ease-in-out;
 `;
 
 const StyledLink = styled(Link)`
   margin-top: 2rem;
   font-size: 1.2rem;
   text-decoration: none;
-  color: #000;
-  border: 1px solid #000;
+  color: #f2e8dc;
+  border: 1px solid #f2e8dc;
   padding: 0.5rem 1rem;
   border-radius: 4px;
-  background-color: white;
+  background-color: transparent;
+  animation: ${fadeIn} 3s ease-in-out;
 `;
 
 const PicturePage = () => {
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    setShowContent(true);
+  }, []);
+
   return (
     <Container>
-      <Text>PlantPilot</Text>
-      <StyledLink href="/plants">View Plants</StyledLink>
+      {showContent && (
+        <>
+          <PlantPilotText>PlantPilot</PlantPilotText>
+          <h2 style={{ color: "#f2e8dc", fontWeight: "normal" }}>
+            Discover the perfect plants for your garden
+          </h2>
+          <StyledLink href="/plants">View Plants</StyledLink>
+        </>
+      )}
     </Container>
   );
 };
